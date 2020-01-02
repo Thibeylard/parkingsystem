1da264b6c452e1aac8840f95dfe9f7434768bc77
Thibeylard <17107365+Thibeylard@users.noreply.github.com>
2020-01-02 07:26:09 +0100

    Fixed assertEquals mixed expected/actual results

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -73,7 +73,7 @@ public class FareCalculatorServiceTest {
         ticket.setOutTime(outTime);
         ticket.setParkingSpot(parkingSpot);
         fareCalculatorService.calculateFare(ticket);
-        assertEquals(ticket.getPrice(), Fare.BIKE_RATE_PER_HOUR);
+        assertEquals(Fare.BIKE_RATE_PER_HOUR,ticket.getPrice());
     }
 
     /**
